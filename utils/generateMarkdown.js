7885e7b944fd1e7bd4d859be1393bd39f4f8e377
Shannon Sagehorn <srsagehorn@gmail.com>
2020-09-08 00:29:34 -0500

    fixed read me render issues

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  #${data.title}
+  # ${data.title}
 
-  ##Description
+  ## Description
   ${data.description}
 
-  ##Table of Contents
+  ## Table of Contents
     1. Installation
     2. Usage
     3.Credits
@@ -14,22 +14,22 @@ function generateMarkdown(data) {
     5.Contributing
     
 
-  ##Installation
+  ## Installation
   ${data.install}
 
-  ##Usage
+  ## Usage
   ${data.usage}
 
-  ##Credits
+  ## Credits
   ${data.contributors}
 
-  ##License
+  ## License
   ${data.license}
 
-  ##Contributing
+  ## Contributing
   If you would like to contribute to this project reach out to me via email at ${data.email}
 
-  ##Tests
+  ## Tests
   ${data.tests}
 
   If you have any questions you can contact me at ${data.email} or reach out on linkedIn https://www.linkedin.com/in/shannonsagehorn/
